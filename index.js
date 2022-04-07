e4da5b892c3afeab41d35212611f95af4aab9ec3
felipehdev <felipehdev@gmail.com>
2022-04-07 19:35:23 -0300

    plano b

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
 
 //B64 FUNCTIONS
 
-// var entry = document.getElementById("entry");
-// var entry = entry.value;
-var entry = 'Sulzc2ljYQ=='
+ var entry = document.getElementById("entry").value;
+ console.log(entry);
 
-// var result = document.getElementById("result");
-// var result = result.value;
+ // var result = document.getElementById("result").value;
 
 
-//Base64 functions
-function b64Code(entry) {
-  var toB64 = window.btoa(entry);
-  console.log(window.btoa(entry));
-}
-
-function b64Decode(entry) {
-  var fromB64 = window.atob(entry);
-  console.log(window.atob(entry));
-}
-
-function b64EncodeDecode(params) {
-  if (document.getElementById("code").checked) {
-    b64Code(entry);
-    //imprimir resultado no textarea de baixo
-  } else if (document.getElementById("decode").checked) {
-    b64Decode(entry);
-    //imprimir resultado no textarea de baixo
+//Base64 FUNCTION
+function b64EncodeDecode() {
+  if (document.getElementById("code").checked) {    
+    var toB64 = window.btoa(entry);
+    document.getElementById('paragrafo').innerHTML = toB64;
+    
+  } else {
+    var fromB64 = window.atob(entry);
+    document.getElementById('paragrafo').innerHTML = fromB64;
+    
   }
 }
-b64Code(entry)
-b64Decode(entry)
 
 //CESAR FUNCTIONS
-var entry = 'joao JOAO Z';
 var entryArray = entry.split("");
 
 console.log(entryArray);
 
 
-//asci converter & incrementer
+//asci converter & INCREMENTER
 var converted = [];
-function asciiConverter(increment) {
+function asciiIncrementer(increment) {
   for (let i = 0; i < entryArray.length; i++) {
     
     asciiResults = entryArray[i].charCodeAt();
@@ -60,14 +47,40 @@ function asciiConverter(increment) {
       
     }
     else if (asciiResults == 32) {
-      asciiResults = 32;
+      asciiResults;
     }
     
     converted.push(asciiResults)    
   }
 }
-asciiConverter(2)
-console.log(converted);
+
+//asci converter & DECREMENTER
+var converted = [];
+function asciiDecrementer(increment) {
+  for (let i = 0; i < entryArray.length; i++) {
+    
+    asciiResults = entryArray[i].charCodeAt();
+    console.log(asciiResults);
+
+    if (192 <= asciiResults) {
+      asciiResults;
+    }
+
+    else if ( 97 <= asciiResults) {
+      asciiResults = ((asciiResults - 97 - increment) % 26) + 97
+    }
+        
+    else if (65 <= asciiResults) {
+      asciiResults = ((asciiResults - 65 - increment) % 26) + 65;
+      
+    }
+    else if (asciiResults == 32) {
+      asciiResults;
+    }
+    
+    converted.push(asciiResults)    
+  }
+}
 
 //back to unicode
 
@@ -84,7 +97,6 @@ function toUnicode() {
 toUnicode()
 
 // array to string
-
 var exitString = '';
 console.log(exitString);
 
@@ -96,6 +108,36 @@ function arrToString() {
   
 }
 arrToString()
+
+
+//BOTÃO
+
+var go = document.getElementById('goBtn');
+goBtn.addEventListener('click' , start);
+
+var cryptOpts = document.getElementById('crypts')
+cryptOpts.addEventListener('change', start)
+
+
+function start(event) {
+  event.preventDefault();
+
+  var option1 = document.getElementById('base64Crypt')
+  var option2 = document.getElementById('cesarCrypt')
+
+  if (option1.value == 'base64') {
+    b64EncodeDecode();    
+  }
+  else if (option2.value == "cesar" && document.getElementById("code").checked) {
+    asciiConverter();
+    toUnicode();
+    arrToString();        
+  }
+  else if (option2.value == "cesar" && document.getElementById("decode").checked) {
+    
+  }
+  
+}
 
 
 
